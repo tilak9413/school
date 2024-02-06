fb8821b09871e468092cbeef907342a6f9b9b906
Praveen3016 <143151914+Praveen3016@users.noreply.github.com>
2024-02-06 12:01:40 +0530

    home page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Login from "./components/Login";
+import Home from './components/home.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<App />} >
-      <Route path='login' element={<Login />} />
+      <Route path='/' element={<Home />} >
+
       </Route>
       <Route path='login' element={<Login />} />
     </>
