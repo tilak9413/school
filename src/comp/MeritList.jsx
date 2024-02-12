49c73f448079c0dcaae8ec6c7a9ab2b22efd1feb
Praveen3016 <143151914+Praveen3016@users.noreply.github.com>
2024-02-12 11:31:07 +0530

    fdhdh

--- src/comp/MeritList.jsx
+++ src/comp/MeritList.jsx
@@ -10,7 +10,7 @@ function MeritList() {
         <h6 className='text-success my-3 fw-semibold '> Check Marit List :</h6>
         <form >
           <div className="row px-sm-5 mx-sm-5   ">
-          <div className="d-flex gap-3 align-items-center col-sm-6  ">
+          <div className="d-flex gap-3 align-items-center col-sm-6 mb-2 ">
             <label className='m-0 ' htmlFor="">Application No. :</label>
             <input type="text" placeholder='Enter Application No... '  className='inputit w-sm-50  '  />
           </div>
@@ -20,44 +20,71 @@ function MeritList() {
             <input type="text" placeholder='Enter Application No... '  className='inputit w-50'  />
           </div>
 
-          <div className="d-flex gap-3 align-items-center col-sm-6 ">
+          <div className="d-flex gap-3 align-items-center col-sm-6  ">
             <label className='m-0' htmlFor="">Captcha Code :</label>
-            <input type="text" placeholder='Enter Application No... '  className='inputit w-50'  />
+            <input type="text" placeholder='Enter Application No... '  className='inputit w-50 mx-2'  />
           </div>
           </div>
         </form>
         <hr />
-        
-        <table class="table rounded bg-primary">
-  <thead>
-    <tr>
-      <th >#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+        <table className='table bg-white ' >
+              <tr className='toptr'>
+                <th style={{ width: "2%" }}>SR.NO </th>
+                <th>GR NO.</th>
+                <th>Student Name</th>
+                <th>Father Name</th>
+                <th>Date Of Birth</th>
+                <th>Blood Group</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Roland Mendel</td>
+                <td>Austria</td>
+                <td>Ernst Handel</td>
+                <td>Roland Mendel</td>
+                <td>Austria</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Yoshi Tannamuri</td>
+                <td>Canada</td>
+                <td>Laughing Bacchus Winecellars</td>
+                <td>Yoshi Tannamuri</td>
+                <td>Canada</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>Giovanni Rovelli</td>
+                <td>Italy</td>
+                <td>Magazzini Alimentari Riuniti</td>
+                <td>Giovanni Rovelli</td>
+                <td>Italy</td>
+              </tr>
+            </table>
       </div>
     </div>
   )
