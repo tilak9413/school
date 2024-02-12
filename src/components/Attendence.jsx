5a2ba47eda2bec2ee62b9f59d0fbea9cb6dd9b5f
mohit suthar <142993790+mohitsuther@users.noreply.github.com>
2024-02-12 14:25:09 +0530

    change

--- src/components/Attendence.jsx
+++ src/components/Attendence.jsx
@@ -1,10 +1,27 @@
+// import React, { useState } from 'react'
 import React from 'react'
-
+import '../assets/styledashboradcss/apply Attendance.css'
+import Navbar from '../comp/Navbar'
+import Attendancestudent from '../comp/Atendencestudent'
+import Attendancestaff from '../comp/Atendencestaf'
 function Attendence() {
+  const [active,setactive]=React.useState()
   return (
-    <div>
-      Attendence
+    <>
+    <div className='container-fluid'>
+      <Navbar />
+      <div className="d-flex welcome-bar bgcolordash rounded p-2  mx-2">
+        <span className={`smallbolding border-end px-2 pointer ${active == 1 ? "activeaddmition" : ""} `} onClick={() => setactive(1)}>Student Attendance </span>
+        <span className={`smallbolding border-end px-2 pointer ${active == 2 ? "activeaddmition" : ""} `}onClick={ () => setactive(2)} > Staff Attendance</span>
+       
+      </div>
+      {
+        active == 1 ? <Attendancestudent/> :  <Attendancestaff/>
+      }
+     
+    
     </div>
+  </>
   )
 }
 
