6994cadda36f9acd21512530f274337f7f22378a
SumeetRawal <sumeetrawal2@gmail.com>
2024-02-23 13:57:58 +0530

    merit

--- src/components/Fees.jsx
+++ src/components/Fees.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import '../assets/styledashboradcss/Fees.css'
 
 function Fees() {
   return (
     <>
-       <div className="box">
-        
-       </div>
+      
     </>
   )
 }
